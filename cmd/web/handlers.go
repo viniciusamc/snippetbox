e0a845b00ec4e9a69ceae5ef983353d9f11b346e
Vinicius <viniciusmartinsdacunha1@gmail.com>
2024-01-28 19:12:37 -0300

    go study

---
+++ cmd/web/handlers.go
@@ -0,0 +1,86 @@
+package main
+
+import (
+	"fmt"
+	// "html/template"
+	"net/http"
+	"strconv"
+	"vinicius/snippetbox/pkg/models"
+)
+
+func (app *application) home(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+        app.notFound(w)
+		return
+	}
+
+    s, err := app.snippets.Latest()
+    if err != nil {
+        app.serverError(w, err)
+        return
+    }
+
+    for _, snippet := range s {
+        fmt.Fprintf(w, "%v\n", snippet)
+    }
+
+    // files := []string{
+    //     "./ui/html/home.page.tmpl",
+    //     "./ui/html/base.layout.tmpl",
+    //     "./ui/html/footer.partial.tmpl",
+    // }
+    //
+    // ts, err := template.ParseFiles(files...)
+    // if err != nil {
+    //     app.serverError(w, err)
+    //     return
+    // }
+
+    // err = ts.Execute(w, nil)
+    // if err != nil {
+    //     app.serverError(w, err)
+    // }
+}
+
+func (app *application) showSnippet(w http.ResponseWriter, r *http.Request) {
+	id, err := strconv.Atoi(r.URL.Query().Get("id"))
+
+	if err != nil || id < 1 {
+        app.notFound(w)
+		return
+	}
+
+    s, err := app.snippets.Get(id)
+
+    if err == models.ErrNoRecord {
+        app.notFound(w)
+        return
+    } else if err != nil {
+        app.serverError(w, err)
+        return
+    }
+
+    fmt.Fprintf(w, "%v", s)
+}
+
+func (app *application) createSnippet(w http.ResponseWriter, r *http.Request) {
+
+	if r.Method != "POST" {
+		w.Header().Set("Allow", "POST")
+        app.clientError(w, http.StatusMethodNotAllowed)
+		return
+	}
+
+    title := "O snail"
+    content := "O snail \nClimb Mountain"
+    expires := "7"
+
+    id, err := app.snippets.Insert(title, content, expires)
+    if err != nil {
+        app.serverError(w, err)
+        return
+    }
+
+    http.Redirect(w, r, fmt.Sprintf("/snippet?id=%d", id), http.StatusSeeOther)
+
+}
